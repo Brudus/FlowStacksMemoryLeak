95a9c4ec5ae7f75a84f9cc4eba0b9b23f2b8e950
Manuel Schulze <freelance@manuelschulze.com>
2024-01-18 14:26:05 +0100

    Initial Commit

---
+++ FlowStacksMemoryLeak/FlowStacksMemoryLeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowStacksMemoryLeakApp.swift
+//  FlowStacksMemoryLeak
+//
+//  Created by Manuel Schulze on 18.01.24.
+//
+
+import SwiftUI
+
+@main
+struct FlowStacksMemoryLeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
